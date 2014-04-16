198dc1e2106bbceb25c5258d61a8be30ef96bd18
Metabolic24 <metabolic.ewilan@hotmail.fr>
2014-04-16 16:35:30 +0200

    Add bootstrap values

--- BiblioJ_JEE/grails-app/conf/BootStrap.groovy
+++ BiblioJ_JEE/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,34 @@
-class BootStrap {
+import biblioj_jee.Livre
+import biblioj_jee.Typedoc;
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+class BootStrap {
+	
+	def init = { servletContext ->
+		
+		Typedoc type1 = new Typedoc(
+			intitule:'test')
+		type1.save()
+		
+		Typedoc type2 = new Typedoc(
+			intitule:'revue')
+		type2.save()
+		
+		Livre livre1 = new Livre(
+			titre: 'Les dents de la mer',
+			nombreExemplaires: 2,
+			nombreExemplairesDisponibles: 2,
+			type: type1)
+		livre1.save()
+		
+		Livre livre2 = new Livre(
+			titre: 'Germinal',
+			nombreExemplaires: 2,
+			nombreExemplairesDisponibles: 2,
+			type: type1)
+		livre2.save()
+	}
+	
+	def destroy = {
+	}
 }
+	
